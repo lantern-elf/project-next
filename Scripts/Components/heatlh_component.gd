fcb5812b7f2dc8f7f0d8f8eaf2be2b690b8db62a
Lantern Elf <lantern4k@gmail.com>
2026-07-10 09:59:02 +0700

    adding discord addons and adding hit_flash

--- Scripts/Components/heatlh_component.gd
+++ Scripts/Components/heatlh_component.gd
@@ -2,7 +2,7 @@ extends Node
 class_name HealthComponent
 
 @export var body: CharacterBody2D
-
+@export var body_sprite: Sprite2D
 @export var max_health = 3.0
 @export var current_health = 3.0
 
@@ -11,6 +11,7 @@ signal get_damage
 func take_damage(amount: float):
 	current_health -= amount
 	get_damage.emit()
+	flash_hit()
 	if current_health <= 0:
 		die()
 
@@ -21,3 +22,8 @@ func heal(amount: float):
 
 func die():
 	body.queue_free()
+
+func flash_hit():
+	body_sprite.material.set_shader_parameter("hit_flash_on", true)
+	await get_tree().create_timer(.2, true, false, true).timeout
+	body_sprite.material.set_shader_parameter("hit_flash_on", false)
